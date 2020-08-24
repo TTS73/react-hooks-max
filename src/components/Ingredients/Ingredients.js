4245d8f29e410354a43d9d6daaf62f12e9f0bdc7
Tero Salomaki <tero.salomaki@vincit.fi>
2020-08-24 17:56:48 +0300

    After lecture 442

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import ErrorModaö from './../UI/ErrorModal';
 import Search from './Search';
+import ErrorModal from './../UI/ErrorModal';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ userIngredients, setUserIngredients ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ error, setError ] = useState();
+  // useEffect(() => {
+  //   fetch('https://react-hooks-update-122e0.firebaseio.com/ingredients.json')
+  //     .then(response => response.json())
+  //     .then(responseData => {
+  //       const loadedIngredients = [];
+  //       for (const key in responseData) {
+  //         loadedIngredients.push({
+  //           id: key,
+  //           title: responseData[key].title,
+  //           amount: responseData[key].amount
+  //         })
+  //       }
+  //       setUserIngredients(loadedIngredients);
+  //     })
+  //   }, []);
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    setIsLoading(true);
+    fetch('https://react-hooks-update-122e0.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      setIsLoading(false);
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients,
+        { id: responseData.name, ...ingredient }
+      ]);
+    })
+
+  }
+
+  const removeIngredientHandler = id => {
+    setIsLoading(true);
+    fetch(`https://react-hooks-update-122e0.firebaseio.com/ingredients/${id}.json`, {
+      method: 'DELETE'
+    }).then (response => {
+      setIsLoading(false);
+      setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== id));
+    }).catch(error => {
+      setError(error.message);
+      setIsLoading(false);
+    });
+  }
+
+  const clearError = () => {
+    setError(null);
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
-
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal> }
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
